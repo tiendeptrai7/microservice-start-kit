f78f1b4ddb45f9ba3f31d3c06ebc230a518b08f7
vfa-tienpt <phanthanhtientcl@gmail.com>
2024-04-19 19:29:07 +0700

    add swagger

--- nestjs-microservices/module-first/src/module/user/user.controller.ts
+++ nestjs-microservices/module-first/src/module/user/user.controller.ts
@@ -2,14 +2,25 @@ import { Controller, Get } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { UserService } from './user.service';
 import { Users } from '../../entities/users.entity';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserDto } from './dto';
 
 @Controller('users')
+@ApiTags('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @MessagePattern({ cmd: 'get_hello' })
   @Get()
-  getUsers(): Promise<any[]> {
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successful response',
+    type: [UserDto],
+  })
+  @ApiResponse({ status: 400, description: 'Bad request' })
+  @ApiResponse({ status: 404, description: 'Not found' })
+  getUsers(): Promise<Users[]> {
     return this.userService.getUsers();
   }
 }
